be64ca5307b75a7d2ca581e069b7ab69aa0e5cf6
Shahriar <77535132+Shahriar-0@users.noreply.github.com>
2022-02-26 23:53:47 -0800

    Create A2_Q1.cpp

---
+++ A2_Q1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int num_of_changes(int index) {
+    if (index == 1)
+        return 0;
+    //if it's divisible by two then it's the opposite of ceil(index / 2)
+    //if not the same
+    return !(index % 2) + num_of_changes(ceil(index / 2.0));
+}
+
+int main(void) {
+    int numOfLevel, index;
+    cin >> numOfLevel >> index;
+    (num_of_changes(index) % 2)? cout << "1\n" : cout << "0\n"; 
+}
