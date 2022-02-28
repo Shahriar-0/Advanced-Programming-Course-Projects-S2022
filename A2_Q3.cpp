15499b58c51dda8406d55dbe64146d662b83cc23
Shahriar <77535132+Shahriar-0@users.noreply.github.com>
2022-02-28 11:43:15 -0800

    Update A2_Q3.cpp

--- A2_Q3.cpp
+++ A2_Q3.cpp
@@ -45,6 +45,12 @@ Point generate_new_coordinate(Point position, Direction direction) {
     return position;
 }
 
+Point generate_previous_coordinate(Point position, Direction direction) {
+    position.y -= (direction == RIGHT) - (direction == LEFT);
+    position.x -= (direction == DOWN) - (direction == UP);
+    return position;
+}
+
 Edge_status hit_edge(Point position, int numOfRows, int numOFColumns) {
     if (position.x == numOfRows)
         return BELOW_EDGE;
@@ -66,29 +72,38 @@ bool in_loop(const vector<vector<Coordinate>>& map, Direction direction, Point p
     }
 }
 
+void mark_position(vector<vector<Coordinate>>& map, Point position, Direction direction) {
+    switch (direction) {
+            case RIGHT: map[position.x][position.y].right = SEEN; break;
+            case LEFT: map[position.x][position.y].left = SEEN; break;
+            case UP: map[position.x][position.y].up = SEEN; break;
+            case DOWN: map[position.x][position.y].down = SEEN; break;
+    }
+}
+
 int find_shortest_path(vector<vector<Coordinate>> map, Direction direction, Point position, int length = 0) {
     Edge_status hittedEdge;
     bool inLoop;
     do {
         //we go ahead till we hit rock or edge or we stuck in a loop
-        switch (direction) {
-            case RIGHT: map[position.x][position.y].right = SEEN; break;
-            case LEFT: map[position.x][position.y].left = SEEN; break;
-            case UP: map[position.x][position.y].up = SEEN; break;
-            case DOWN: map[position.x][position.y].down = SEEN; break;
-        }
-        
+        mark_position(map, position, direction);
         position = generate_new_coordinate(position, direction);
         length++;
         hittedEdge = hit_edge(position, map.size(), map[0].size());
+        if (hittedEdge)
+            break;
         inLoop = in_loop(map, direction, position);
-    } while (!hittedEdge && map[position.x][position.y].data == EMPTY && !inLoop);
+    } while (!inLoop && map[position.x][position.y].data == EMPTY);
+
+    if (inLoop) 
+        //we are stuck in a loop so we
+        //just return random big number
+        return BIG_NUMBER;
 
     if (hittedEdge) {
         //go to previous position
         length--;
-        position.y -= (direction == RIGHT) - (direction == LEFT);
-        position.x -= (direction == DOWN) - (direction == UP);
+        position = generate_previous_coordinate(position, direction);
         //check the other two
         if (hittedEdge == RIGHT_EDGE || hittedEdge == LEFT_EDGE) 
             return min(find_shortest_path(map, UP, position, length), find_shortest_path(map, DOWN, position, length));
@@ -97,19 +112,10 @@ int find_shortest_path(vector<vector<Coordinate>> map, Direction direction, Poin
             return min(find_shortest_path(map, LEFT, position, length), find_shortest_path(map, RIGHT, position, length));
     }
 
-    if (inLoop) 
-        //we are stuck in a loop so we
-        //just return random big number
-        return BIG_NUMBER;
-    if (map[position.x][position.y].data == BAMBOO)
-        //finally found
-        return length;
-
     if (map[position.x][position.y].data == ROCK) {
         //go to previous position
         length--;
-        position.y -= (direction == RIGHT) - (direction == LEFT);
-        position.x -= (direction == DOWN) - (direction == UP);
+        position = generate_previous_coordinate(position, direction);
         //check the other two
         if (direction == RIGHT || direction == LEFT)
             return min(find_shortest_path(map, UP, position, length), find_shortest_path(map, DOWN, position, length));
@@ -117,6 +123,12 @@ int find_shortest_path(vector<vector<Coordinate>> map, Direction direction, Poin
         else 
             return min(find_shortest_path(map, LEFT, position, length), find_shortest_path(map, RIGHT, position, length));
     }
+
+
+    if (map[position.x][position.y].data == BAMBOO)
+        //finally found
+        return length;
+
 }
 
 int main(void) {
@@ -124,7 +136,6 @@ int main(void) {
     Direction FIRST_INITIAL_DIRECTION = RIGHT, SECOND_INITIAL_DIRECTION = DOWN;
     vector<vector<Coordinate>> map;
     getting_input(map);
-    cout << endl;
     cout << min(find_shortest_path(map, FIRST_INITIAL_DIRECTION, PANDA_POSITION),
          find_shortest_path(map, SECOND_INITIAL_DIRECTION, PANDA_POSITION)) << endl;
 }
