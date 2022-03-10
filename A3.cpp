1055b7fce0c39df46558065ad32a47aad123c2fa
Shahriar <77535132+Shahriar-0@users.noreply.github.com>
2022-03-10 01:04:13 -0800

    Update A3.cpp

--- A3.cpp
+++ A3.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <vector>
 #include <fstream>
+#include <iomanip>
+#include <algorithm>
 
 #define NO_TRANSLATOR_FOUND "Not Found"
 #define LANGUAGE_NOT_FOUND -1
+#define SPLIT_CHAR " "
 
 using namespace std;
 
@@ -45,25 +49,56 @@ typedef vector<Event> Events;
 
 
 void get_input(string, Languages&, Translators&, Events&);
-void get_input_and_convert_to_suitable_format(string, Languages&, Translators&, Events&);
+void initialising_data(string, Languages&, Translators&, Events&);
 void check_for_file_validation(ifstream&);
 void read_languages(ifstream&, Languages&, Translators&);
-Time convert_ints_to_Time_format(int, int);
+Time read_time_format(ifstream&);
 Translator read_translator_info(ifstream&);
 int search(const Languages&, string);
 void add_new(Languages&, string, Translator, Translators&);
 void add_existing(Languages&, string, Translator, Translators&, int);
 void read_events(ifstream&, Events&);
+vector<string> read_list_of_languages(ifstream&);
+vector<string> string_tokenizer(char*);
+
+void print_everything(Languages languageslist, Translators translatorsList, Events eventsList) {
+    cout << "languages " << endl;
+    for (auto x : languageslist) {
+        cout << x.name << " with " << x.numOfTranslators << " translators " << endl;
+        cout << "list of translators " << endl;
+        for (auto y : x.translators) {
+            cout << y->name << " from " << setw(2) << setfill('0') << y->startTime.hour << ":";
+            cout << setw(2) << setfill('0') << y->startTime.minute << " to ";
+            cout << setw(2) << setfill('0') << y->endTime.hour << ":";
+            cout << setw(2) << setfill('0') << y->endTime.minute << " to ";
+            cout << "knowing " << y->numOfLanguages << " languages" << endl;
+        }
+    }
+    cout << endl <<"************now events****************" << endl;
+    for (auto x : eventsList) {
+        cout << x.name << " which starts at " << setw(2) << setfill('0') << x.startTime.hour << ":";
+        cout << setw(2) << setfill('0') << x.startTime.minute << " to ";
+        cout << setw(2) << setfill('0') << x.endTime.hour << ":";
+        cout << setw(2) << setfill('0') << x.endTime.minute << "  ";
+        cout << "with this required languages ";
+        for (auto y : x.eventLanguages) {
+            cout << y.languageName << " ";
+        }
+        cout << endl;
+    }
+}
 
 
 int main(int argc, const char* argv[]) {
     Languages languagesList;
     Translators translatorsList;
     Events eventsList;
+    initialising_data(argv[argc - 1], languagesList, translatorsList, eventsList);
+    print_everything(languagesList, translatorsList, eventsList);
 }
 
 
-void get_input_and_convert_to_suitable_format(string inputFile, Languages& languagesList,
+void initialising_data(string inputFile, Languages& languagesList,
     Translators& translatorsList, Events& eventsList) {
     get_input(inputFile, languagesList, translatorsList, eventsList);
 
@@ -76,10 +111,12 @@ void check_for_file_validation(ifstream& inputFile) {
     }
 }
 
-Time convert_ints_to_Time_format(int hour, int minute) {
+Time read_time_format(ifstream& fileStream) {
     Time time;
-    time.hour = hour;
-    time.minute = minute;
+    fileStream >> time.hour;
+    char letter;
+    fileStream >> letter;
+    fileStream >> time.minute;
     return time;
 }
 
@@ -94,16 +131,14 @@ int search(const Languages& languages, string name) {
 Translator read_translator_info(ifstream& fileStream) {
     Translator translator;
     fileStream >> translator.name;
-    int startHour, endHour, startMinute, endMinute;
-    fileStream >> startHour>> startMinute >> endHour  >> endMinute;
-    translator.startTime = convert_ints_to_Time_format(startHour, startMinute);
-    translator.endTime = convert_ints_to_Time_format(endHour, endMinute);
+    translator.startTime = read_time_format(fileStream);
+    translator.endTime = read_time_format(fileStream);
     translator.numOfLanguages = 1; //at least one language is known by that translator
     return translator;
 }
 
 void add_new(Languages& languagesList, string name, Translator translator, Translators& translatorsList) {
-    if (translator.name != translatorsList[translatorsList.size() - 1].name)    
+    if (translatorsList.empty() || translator.name != translatorsList[translatorsList.size() - 1].name)    
         translatorsList.push_back(translator);
     else
         translatorsList[translatorsList.size() - 1].numOfLanguages++; //in case someone knows more than one language
@@ -115,7 +150,7 @@ void add_new(Languages& languagesList, string name, Translator translator, Trans
 }
 
 void add_existing(Languages& languagesList, string name, Translator translator, Translators& translatorsList, int index) {
-    if (translator.name != translatorsList[translatorsList.size() - 1].name)    
+    if (translatorsList.empty() || translator.name != translatorsList[translatorsList.size() - 1].name)    
         translatorsList.push_back(translator);
     else
         translatorsList[translatorsList.size() - 1].numOfLanguages++; //in case someone knows more than one language
@@ -124,20 +159,39 @@ void add_existing(Languages& languagesList, string name, Translator translator, 
     languagesList[index].translators.push_back(&translatorsList[translatorsList.size() - 1]);
 }
 
+vector<string> string_tokenizer(char* str) {
+    vector<string> result;
+    str = strtok(str, SPLIT_CHAR);
+    while (str != NULL) {
+        string singleLanguageName(str);
+        result.push_back(singleLanguageName);
+        str = strtok(NULL, SPLIT_CHAR);
+    }
+    return result;
+}
+
+vector<string> read_list_of_languages(ifstream& fileStream) {
+    string languagesName;
+    getline(fileStream, languagesName);
+    languagesName = languagesName.substr(1); //cause we have an extra space
+    return string_tokenizer(&languagesName[0]);
+}
+
 void read_languages(ifstream& fileStream, Languages& languagesList, Translators& translatorsList) {
     
     int lineCount; 
     fileStream >> lineCount;
+    translatorsList.resize(lineCount);
     while (lineCount--) {
         Translator translator = read_translator_info(fileStream);
-        string languageName;
-        while (fileStream >> languageName) {
-            int index = search(languagesList, languageName);
+        vector<string> splittedLanguages = read_list_of_languages(fileStream);
+        for (auto singleLanguageName : splittedLanguages) {
+            int index = search(languagesList, singleLanguageName);
 
             if (index == LANGUAGE_NOT_FOUND) 
-                add_new(languagesList, languageName, translator, translatorsList);
+                add_new(languagesList, singleLanguageName, translator, translatorsList);
             else
-                add_existing(languagesList, languageName, translator, translatorsList, index);
+                add_existing(languagesList, singleLanguageName, translator, translatorsList, index);
         }
     }
 }
@@ -149,13 +203,15 @@ void read_events(ifstream& fileStream, Events& eventsList) {
     while (lineCount--) {
         Event event;
         fileStream >> event.name;
-        int startHour, endHour, startMinute, endMinute;
-        fileStream >> startHour>> startMinute >> endHour  >> endMinute;
-        event.startTime = convert_ints_to_Time_format(startHour, startMinute);
-        event.endTime = convert_ints_to_Time_format(endHour, endMinute);
+        event.startTime = read_time_format(fileStream);
+        event.endTime = read_time_format(fileStream);
+        vector<string> splittedLanguages = read_list_of_languages(fileStream);
         EventsLanguage eventLanguage = {""};
-        while (fileStream >> eventLanguage.languageName)
+        for (auto singleLanguageName : splittedLanguages) { 
+            eventLanguage.languageName = singleLanguageName;
             event.eventLanguages.push_back(eventLanguage);
+        }
+        
         eventsList.push_back(event);
     }
 }
