06e9c70254230880ffa030077530b26efa67e594
Shahriar <77535132+Shahriar-0@users.noreply.github.com>
2022-03-24 09:24:31 -0400

    Update solution.cpp

--- A4/solution.cpp
+++ A4/solution.cpp
@@ -3,91 +3,101 @@
 
 using namespace std;
 
-vector<vector<int> > farm;
-//----------------
-long int
-Sum(
-    vector<int> v
-) {
-     long int sum = 0;
+#define NUM_OF_MOVES 4
+
+long int sum_of_a_row(const vector<int>& v) {
+    long int sum = 0;
     for (int i = 0; i < v.size(); i++)
-    sum += v[i];
+        sum += v[i];
     return sum;
 }
-//--------------------------------
-void solve(int m, int n, long int& harvest){
+
+long int sum_of_a_column(const vector<vector<int>>& farm, int column) {
     long int sum = 0;
-    int max_row = 0;
-    int maxColumn = 0;
-    int row = 0;
-    int column = 0;
-
-    for(int move = 0; move < 4; move++) {
-        vector<long int> row_sums;
-
-        for(int i = 0; i < m; i++){
-            sum = Sum(farm[i]);
-            row_sums.push_back(sum);
-            sum = 0;
-        }
-
-        for(int i = 0; i < m; i++){
-            if (row_sums[i] > max_row) {
-                max_row = row_sums[i];
-                row = i;
-            }
-        }
-
-        for(int j = 0; j < n; j++){
-            for(int i = 0; i < m; i++){
-                sum += farm[i][j];
-            }
-            if(sum > maxColumn){
-                maxColumn = sum;
-                column = j;
-            }
-            sum = 0;
-        }
-        if(maxColumn > max_row){
-            harvest += maxColumn;
-            for(int i = 0; i < m; i++){
-                farm[i][column] = 0;
-            }
-        }
-        else{
-            harvest += max_row;
-            for(int j = 0; j < n; j++){
-                farm[row][j] = 0;
-            }
-        }
-        row = 0;
-        column = 0;
-        maxColumn = 0;
-        max_row = 0;
-    }
+    for (int i = 0; i < farm.size(); i++)
+        sum += farm[i][column];
+    return sum;
 }
-/*----------------*/
-int main() {
-    int m, n;
-    int    product;
-    
-    long int harvest = 0;
-    cin>>m>> n;
-    farm = vector < vector < int > > (m, vector<int>(n));
-    for(int i = 0 ; i < m ; i++){
-        for(int j = 0 ; j < n ; j++){
-            cin >> product;farm[i][j] = product;
+
+long int find_max_row(const vector<vector<int>>& farm, int rowCount, int& rowIndex) {
+    long int answer = INT32_MIN;
+
+    for (int i = 0; i < rowCount; i++) {
+        long int currentSum = sum_of_a_row(farm[i]);
+
+        if (currentSum > answer) {
+            rowIndex = i;
+            answer = currentSum;
         }
-    
     }
 
-    
-    
-    // solve
-    solve(m, n, harvest);
-    cout<<harvest;
-    return 0;
-    cout << "Hello there! I'm unreachable!" << endl;
-} // main
+    return answer;
+}
 
-// no reason for this comment
+long int find_max_column(const vector<vector<int>>& farm, int columnCount, int& columnIndex) {
+    long int answer = INT32_MIN;
+
+    for (int i = 0; i < columnCount; i++) {
+        long int currentSum = sum_of_a_column(farm, i);
+
+        if (currentSum > answer) {
+            columnIndex = i;
+            answer = currentSum;
+        }
+    }
+
+    return answer;
+}
+
+void harvest_a_row(vector<vector<int>>& farm,  int rowIndex, int columnCount) {
+    for (int i = 0; i < columnCount; i++)
+        farm[rowIndex][i] = 0;
+}
+
+void harvest_a_column(vector<vector<int>>& farm,  int columnIndex, int rowCount) {
+    for (int i = 0; i < rowCount; i++)
+        farm[i][columnIndex] = 0;
+}
+
+long int find_most_profitables(int rowCount, int columnCount,vector<vector<int>>& farm) {
+    int maxRow, maxColumn, rowIndex, columnIndex;
+    long int harvested = 0;
+
+    for (int move = 0; move < NUM_OF_MOVES; move++) {
+        maxRow = find_max_row(farm, rowCount, rowIndex);
+        maxColumn = find_max_column(farm, columnCount, columnIndex);
+
+        if (maxRow > maxColumn) {
+            harvested += maxRow;
+            harvest_a_row(farm, rowIndex, columnCount);
+        }
+        else {
+            harvested += maxColumn;
+            harvest_a_column(farm, columnIndex, rowCount);
+        }
+    }
+
+    return harvested;
+}
+
+vector<vector<int>> get_input(int rowCount, int columnCount) {
+    vector<vector<int>> result(rowCount, vector<int>(columnCount));
+    
+    for (int i = 0; i < rowCount; i++) 
+        for (int j = 0; j < columnCount; j++) 
+            cin >> result[i][j];
+        
+    return result;
+}
+
+int main() {
+    int rowCount, columnCount;
+    cin >> rowCount >> columnCount;
+    
+    vector<vector<int>> farm = get_input(rowCount, columnCount);
+
+    long int harvested = find_most_profitables(rowCount, columnCount, farm);
+    
+    cout << harvested << endl;
+    return 0;
+}
