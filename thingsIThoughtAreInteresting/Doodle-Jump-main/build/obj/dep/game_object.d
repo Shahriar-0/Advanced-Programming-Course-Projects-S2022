ca77699291fa67b734993ba8136bbe0d49db5461
Shahriar-0 <attarshahriar@gmail.com>
2022-04-23 01:04:51 -0700

    "dunno"

---
+++ thingsIThoughtAreInteresting/Doodle-Jump-main/build/obj/dep/game_object.d
@@ -0,0 +1,10 @@
+build/obj/game_object.o: src/game_object.cpp src/game_object.hpp \
+ src/utils/point_f.hpp src/utils/rectangle_f.hpp src/utils/point_f.hpp
+
+src/game_object.hpp:
+
+src/utils/point_f.hpp:
+
+src/utils/rectangle_f.hpp:
+
+src/utils/point_f.hpp:
