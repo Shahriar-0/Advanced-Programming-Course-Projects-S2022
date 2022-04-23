b806ded2e8a586793eb91d00e8309be3d7351a0a
Shahriar-0 <attarshahriar@gmail.com>
2022-04-23 02:11:30 -0700

    "getting ready to fuck"

--- A5-810100186/main.cpp
+++ A5-810100186/main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
-//I'm not sure what this is, but it seems to work
-#include "rsdl.hpp"
+//#include "rsdl.hpp"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
-    
+	cout << "hello world" << endl;	
 }
