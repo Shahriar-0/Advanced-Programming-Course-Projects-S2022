60d4290897055b7574407d3263ffa2232bd7430a
Shahriar-0 <attarshahriar@gmail.com>
2022-04-24 05:52:09 -0700

    sth

--- A5-810100186/codes/star_wars.cpp
+++ A5-810100186/codes/star_wars.cpp
@@ -1,6 +1,17 @@
 #include "star_wars.hpp"
 using namespace std;
 
+void StarWars::read_file(string filename) {
+    vector<string> currentMap;
+    ifstream fileStream (filename);
+    int numOfMaps;
+    fileStream >> numOfMaps;
+    fileStream >> totalHeight >> totalWidth;
+    while (numOfMaps--) {
+        
+    }
+}
+
 StarWars::StarWars(string filename, Window* _win) : musicPlayer(_win) {
     win = _win;
     // win->draw_img("assets/pictures/background/galaxy.jpeg");
--- A5-810100186/codes/star_wars.hpp
+++ A5-810100186/codes/star_wars.hpp
@@ -2,13 +2,15 @@
 #define __STAR_WARS_HPP__
 #include <iostream>
 #include <string>
+#include <vector>
 #include <cstring>
+#include <fstream>
 // #include "enemy_bullet.hpp"
 // #include "my_bullet.hpp"
 // #include "moving_enemies.hpp"
 // #include "moving_enemy.hpp"
 // #include "my_bullet.hpp"
-// #include "spaceship.hpp"
+// #include "spaceship.hpp"s
 // #include "rsdl.hpp"
 // #include "stationary_enemies.hpp"
 // #include "stationary_enemy.hpp"
@@ -17,12 +19,14 @@
 class StarWars {
 public:
     StarWars(std::string, Window*);
+    void read_file(std::string);
     //void run();
 private:
     AudioPlayer musicPlayer;
     Window* win;
     //void initialise();
-
+    std::vector<std::vector<std::string>> maps;
+    int totalHeight, totalWidth;
 };
 
 #endif 
