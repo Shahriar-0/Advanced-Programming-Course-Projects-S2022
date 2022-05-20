6d3eb3706817cc035eb634796c9a28fddbef8087
Shahriar-0 <attarshahriar@gmail.com>
2022-05-20 13:20:45 -0400

    completed error handler

--- A7/error_handler.cpp
+++ A7/error_handler.cpp
@@ -1,1 +1,4 @@
 #include "error_handler.hpp"
+
+ErrorHandler::ErrorHandler(std::string _message, std::string _detail) : invalid_argument(_message), detail(_detail) {}
+void ErrorHandler::show_error() { std::cout << what() << std::endl; }
--- A7/error_handler.hpp
+++ A7/error_handler.hpp
@@ -1,4 +1,16 @@
 #ifndef __ERROR_HANDLER_HPP__
 #define __ERROR_HANDLER_HPP__
 
+#include <iostream>
+#include <stdexcept>
+#include <cstring>
+
+class ErrorHandler : public std::invalid_argument {
+public:
+    ErrorHandler(std::string message, std::string detail);
+    void show_error();
+private:
+    std::string detail;
+};
+
 #endif
