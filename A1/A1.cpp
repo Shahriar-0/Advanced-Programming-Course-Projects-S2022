7ffea6a0bca427f72120a607afc72dcd2cd42f2f
Shahriar-0 <attarshahriar@gmail.com>
2023-07-16 22:20:27 +0330

    Refactored A1

--- A1/A1.cpp
+++ A1/A1.cpp
@@ -1,137 +1,136 @@
-//an app to schedule your activities for you 
-//based on time required and free times that you got
-#include <unordered_map>
+#include <string.h>
+
+#include <iomanip>
 #include <iostream>
 #include <vector>
-#include <string.h>
-#include <iomanip>
 
-#define MINUTES_PER_HOUR 60
-#define START_HOUR 12
-#define START_MINUTE 00
-#define END_HOUR 20
-#define END_MINUTE 00
+namespace IO {
+// clang-format off
+const std::string TIMES_DELIMETER       = "-";
+const std::string HOUR_MINUTE_DELIMETER = ":";
+const std::string END_OF_INPUT          = "#";
+const std::string ID_TIME_DELIMETER     = " ";
+// clang-format on
+const char FILLER = '0';
+} // namespace IO
 
-using namespace std;
-
-typedef struct Time_format {
+namespace Time {
+typedef struct TimeFormat {
     int hour;
     int minute;
-} Time_format;
+} TimeFormat;
 
-typedef struct Time_period {
-    Time_format begin;
-    Time_format end;
-    Time_format currentTime;
+typedef struct TimePeriod {
+    TimeFormat begin;
+    TimeFormat end;
+    TimeFormat currentTime;
     int duration;
     int range_id;
-} Time_period;
+    
+} TimePeriod;
+// clang-format off
+constexpr int MINUTES_PER_HOUR = 60;
+constexpr Time::TimeFormat START_TIME = Time::TimeFormat{12, 00};
+constexpr Time::TimeFormat END_TIME   = Time::TimeFormat{20, 00};
+// clang-format on
+} // namespace Time
 
-Time_period string_to_time_format(char*);
-void getting_input(vector<Time_period>&, vector<Time_period>&);
-int time_diffence(Time_period, Time_period);
-void find_free_times(vector<Time_period>, vector<Time_period>&);
-void showing_results(vector<Time_period>, vector<Time_period>);
-void add_and_wrap_time(Time_format*, int);
 
-int main(void) {    
-    vector<Time_period> occupiedTimes, freeTimes, activites;
-    getting_input(occupiedTimes, activites);
-    find_free_times(occupiedTimes, freeTimes);
-    showing_results(freeTimes, activites);
-    return 0;
+namespace Time {
+int time_difference(Time::TimeFormat first, Time::TimeFormat second) {
+        return (second.hour * Time::MINUTES_PER_HOUR + second.minute) -
+               (first.hour * Time::MINUTES_PER_HOUR + first.minute);
 }
 
-int time_diffence(Time_format first, Time_format second) {
-    return second.hour * MINUTES_PER_HOUR + second.minute - 
-        (first.hour * MINUTES_PER_HOUR + first.minute);
+void find_free_times(std::vector<Time::TimePeriod> occupiedTimes, std::vector<Time::TimePeriod>& freeTimes) {
+    Time::TimeFormat currentTime;
+    currentTime.hour = Time::START_TIME.hour;
+    currentTime.minute = Time::START_TIME.minute;
+    for (auto occupiedTime : occupiedTimes) {
+        if (time_difference(currentTime, occupiedTime.begin) > 0) {
+            Time::TimePeriod period;
+
+            period.currentTime = period.begin = currentTime;
+            period.end = occupiedTime.begin;
+            period.duration = time_difference(currentTime, occupiedTime.begin);
+            period.range_id = 0; // it won't be used so it's not important
+
+            freeTimes.push_back(period);
+        }
+        currentTime = occupiedTime.end;
+    }
 }
 
-Time_period string_to_time_format(char* timeInString) {
-    //just to convert string into a valid format to store
-    //the parses are based on the given input format
-    Time_period time;
-    time.begin.hour = atoi(strtok(timeInString, " :"));
-    time.begin.minute = atoi(strtok(NULL, " -"));
-    time.end.hour = atoi(strtok(NULL, " :"));
-    time.end.minute = atoi(strtok(NULL, " -"));
-    time.duration = time_diffence(time.begin, time.end);
-    time.currentTime = time.begin;
-    time.range_id = 0;
+void time_pass(Time::TimeFormat* time, int duration) {
+    time->minute += duration;
+    while (time->minute >= Time::MINUTES_PER_HOUR) {
+        time->hour++;
+        time->minute -= Time::MINUTES_PER_HOUR;
+    }
+}
 
+TimePeriod string_to_time(char* str) {
+    TimePeriod time;
+    // clang-format off
+    time.begin.hour   = atoi(strtok(str,  IO::HOUR_MINUTE_DELIMETER.c_str()));
+    time.begin.minute = atoi(strtok(NULL, IO::TIMES_DELIMETER.c_str()));
+
+    time.end.hour     = atoi(strtok(NULL, IO::HOUR_MINUTE_DELIMETER.c_str()));
+    time.end.minute   = atoi(strtok(NULL, IO::TIMES_DELIMETER.c_str()));
+
+    time.duration     = time_difference(time.begin, time.end);
+    time.currentTime  = time.begin;
+    time.range_id     = 0;
+    // clang-format on
     return time;
 }
+}  // namespace Time
 
-void getting_input(vector<Time_period>& occupiedTimes, vector<Time_period>& activites) {
-    //a function to get initial values for us
-    string line;
-    Time_period time;
+namespace IO {
+void getting_input(std::vector<Time::TimePeriod>& occupiedTimes, std::vector<Time::TimePeriod>& activities) {
+    std::string line;
+    Time::TimePeriod time;
 
-    //recieving occupied times
-    while (cin >> line && line != "#") {
-        time = string_to_time_format(&line[0]);
+    while (std::cin >> line && line != IO::END_OF_INPUT) {
+        time = Time::string_to_time(&line[0]);
         occupiedTimes.push_back(time);
     }
-    //just to be easier when calculating free times
-    time.end.hour = time.begin.hour = END_HOUR;
-    time.end.minute = time.begin.minute = END_MINUTE;
-    time.duration = time.range_id = 0;
+    // clang-format off
+    time.end.hour   = time.begin.hour   = Time::END_TIME.hour;
+    time.end.minute = time.begin.minute = Time::END_TIME.minute;
+    time.duration   = time.range_id = 0; // these two are not used when time is occupied so it doesn't matter
+    // clang-format on
     occupiedTimes.push_back(time);
 
     time = {0};
-    //recieving activities
-    while (cin >> time.duration && cin >> time.range_id) 
-        activites.push_back(time);
+    while (std::cin >> time.duration && std::cin >> time.range_id)
+        activities.push_back(time);
 }
 
-void find_free_times(vector<Time_period> occupiedTimes, vector<Time_period>& freeTimes) {
-    //now we move through day with currentTime and find free times
+void showing_results(std::vector<Time::TimePeriod> freeTimes, std::vector<Time::TimePeriod> activities) {
+    for (int i = 0; i < activities.size(); i++) {
+        int index = activities[i].range_id - 1;
+        std::cout << i + 1 << IO::ID_TIME_DELIMETER;
 
-    Time_format currentTime;
-    currentTime.hour = START_HOUR;
-    currentTime.minute = START_MINUTE;
-    for (auto element : occupiedTimes) {
-        if (time_diffence(currentTime, element.begin) > 0) {
-            //there's a free time
-            Time_period period;
-            period.currentTime = period.begin = currentTime;
-            period.end = element.begin;
-            period.duration = time_diffence(currentTime, element.begin);
-            period.range_id = 0;
+        std::cout << std::setw(2) << std::setfill(IO::FILLER) << freeTimes[index].currentTime.hour
+                  << IO::HOUR_MINUTE_DELIMETER
+                  << std::setw(2) << std::setfill(IO::FILLER) << freeTimes[index].currentTime.minute
+                  << IO::TIMES_DELIMETER;
 
-            //add it to the list
-            freeTimes.push_back(period);
-        }
-        //we are currently at the end of the last one
-        currentTime = element.end;
+        Time::time_pass(&(freeTimes[index].currentTime), activities[i].duration);
+
+        std::cout << std::setw(2) << std::setfill(IO::FILLER) << freeTimes[index].currentTime.hour
+                  << IO::HOUR_MINUTE_DELIMETER
+                  << std::setw(2) << std::setfill(IO::FILLER) << freeTimes[index].currentTime.minute
+                  << std::endl;
     }
 }
+}  // namespace IO
 
-void showing_results(vector<Time_period> freeTimes, vector<Time_period> activites) {
-    //showing times by going through each activity and give it a time
-    
-    for (int i = 0; i < activites.size(); i++) {
-        int index = activites[i].range_id - 1;
-        //the id of the activity
-        cout << i + 1 << " ";
-
-        cout << setw(2) << setfill('0') << freeTimes[index].currentTime.hour << ":" <<
-                setw(2) << setfill('0') << freeTimes[index].currentTime.minute << "-";
-
-        //to subtract the time spent on the activity from free times
-        add_and_wrap_time(&(freeTimes[index].currentTime), activites[i].duration);
-
-        cout << setw(2) << setfill('0') << freeTimes[index].currentTime.hour << ":" <<
-                setw(2) << setfill('0') << freeTimes[index].currentTime.minute << endl;
-    }
-}
-
-void add_and_wrap_time(Time_format* time, int duration) {
-    //for adding times and making some adjustments
-
-    time->minute += duration;
-    while (time->minute >= MINUTES_PER_HOUR) {
-        time->hour++;
-        time->minute -= MINUTES_PER_HOUR;
-    }
+int main(void) {
+    std::vector<Time::TimePeriod> occupiedTimes, freeTimes, activities;
+    IO::getting_input(occupiedTimes, activities);
+    Time::find_free_times(occupiedTimes, freeTimes);
+    IO::showing_results(freeTimes, activities);
+    return 0;
 }
