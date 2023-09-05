b74f3795a06152c09b3fde6755271d4e48161685
Shahriar-0 <attarshahriar@gmail.com>
2023-09-05 21:09:03 +0330

    columnLimit

--- Bonus/solution.cpp
+++ Bonus/solution.cpp
@@ -10,8 +10,9 @@ long int sum_of_a_row(const std::vector<int>& row) {
 }
 
 long int sum_of_a_column(const std::vector<std::vector<int>>& farm, int column) {
-    return std::accumulate(farm.begin(), farm.end(), 0,
-                           [column](long int sum, const std::vector<int>& row) { return sum + row[column]; });
+    return std::accumulate(
+        farm.begin(), farm.end(), 0,
+        [column](long int sum, const std::vector<int>& row) { return sum + row[column]; });
 }
 
 long int find_max_row(const std::vector<std::vector<int>>& farm, int rowCount, int& rowIndex) {
@@ -24,11 +25,12 @@ long int find_max_row(const std::vector<std::vector<int>>& farm, int rowCount, i
     return sum_of_a_row(*maxRow);
 }
 
-long int find_max_column(const std::vector<std::vector<int>>& farm, int columnCount, int& columnIndex) {
-    auto maxColumn = std::max_element(farm[0].begin(), farm[0].end(),
-                                      [&farm, columnCount](int column1, int column2) {
-                                          return sum_of_a_column(farm, column1) < sum_of_a_column(farm, column2);
-                                      });
+long int find_max_column(const std::vector<std::vector<int>>& farm, int columnCount,
+                         int& columnIndex) {
+    auto maxColumn = std::max_element(
+        farm[0].begin(), farm[0].end(), [&farm, columnCount](int column1, int column2) {
+            return sum_of_a_column(farm, column1) < sum_of_a_column(farm, column2);
+        });
 
     columnIndex = maxColumn - farm[0].begin();
     return sum_of_a_column(farm, *maxColumn);
@@ -39,11 +41,11 @@ void harvest_a_row(std::vector<std::vector<int>>& farm, int rowIndex, int column
 }
 
 void harvest_a_column(std::vector<std::vector<int>>& farm, int columnIndex, int rowCount) {
-    for (int i = 0; i < rowCount; i++)
-        farm[i][columnIndex] = 0;
+    for (int i = 0; i < rowCount; i++) farm[i][columnIndex] = 0;
 }
 
-long int find_most_profitable(int rowCount, int columnCount, std::vector<std::vector<int>>& farm, int numOfMoves) {
+long int find_most_profitable(int rowCount, int columnCount, std::vector<std::vector<int>>& farm,
+                              int numOfMoves) {
     int maxRow, maxColumn, rowIndex, columnIndex;
     long int harvested = 0;
 
@@ -67,8 +69,7 @@ std::vector<std::vector<int>> get_input(int rowCount, int columnCount) {
     std::vector<std::vector<int>> result(rowCount, std::vector<int>(columnCount));
 
     for (int i = 0; i < rowCount; i++)
-        for (int j = 0; j < columnCount; j++)
-            std::cin >> result[i][j];
+        for (int j = 0; j < columnCount; j++) std::cin >> result[i][j];
 
     return result;
 }
