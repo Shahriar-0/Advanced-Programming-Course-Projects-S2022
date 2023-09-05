ea0913da5cc6644d2ad0b36b3e4928a2a1d0a9de
Shahriar-0 <attarshahriar@gmail.com>
2023-09-05 20:42:27 +0330

    wrong choice for unique ptr

--- A3/A3-fix.cpp
+++ A3/A3-fix.cpp
@@ -48,6 +48,7 @@ struct Event {
     std::string name;
     TimePeriod timePeriod;
     std::vector<EventsLanguage> eventLanguages;
+    std::vector<std::string> languages;
 };
 
 struct Language {
@@ -79,6 +80,7 @@ void readInput(Data::EventScheduler& scheduler, const std::string fileName) {
         throw std::runtime_error(ERROR_1);
 
     readTranslators(scheduler, file);
+    readEvents(scheduler, file);
 }
 
 void readTranslators(Data::EventScheduler& scheduler, std::ifstream& file) {
@@ -97,6 +99,29 @@ void readTranslators(Data::EventScheduler& scheduler, std::ifstream& file) {
     }
 }
 
+void readEvents(Data::EventScheduler& scheduler, std::ifstream& file) {
+    int numOfEvents;
+    file >> numOfEvents;
+    scheduler.events.reserve(numOfEvents);
+    for (int i = 0; i < numOfEvents; ++i) {
+        Data::Event event;
+        file >> event;
+        Data::EventsLanguage eventLanguage = {""};
+        int priority = 0;
+        for (auto language : event.languages) {
+            eventLanguage.languageName = language;
+            int index = Scheduling::search(scheduler.languages, language);
+            if (index == LANGUAGE_NOT_FOUND)
+                eventLanguage.numOfTranslators = 0;
+            else
+                eventLanguage.numOfTranslators = scheduler.languages[index].numOfTranslators;
+            eventLanguage.priority = priority++;  // this is for knowing the order of input
+            sort(event.eventLanguages.begin(), event.eventLanguages.end(), Scheduling::language_of_event_compare);
+            event.eventLanguages.push_back(eventLanguage);
+        }
+    }
+}
+
 void addNew(Data::EventScheduler& scheduler, std::string name, int translatorIndex) {
     scheduler.languages.push_back(Data::Language(name, 1, {std::make_unique<Data::Translator>(scheduler.translators[translatorIndex])}));
 }
@@ -106,16 +131,28 @@ void addExisting(Data::EventScheduler& scheduler, int index, int translatorIndex
     scheduler.languages[index].translators.push_back(std::make_unique<Data::Translator>(scheduler.translators[translatorIndex]));
 }
 
+std::ifstream& operator>>(std::ifstream& file, Data::Time& time) {
+    file >> time.hour;
+    file >> time.minute;
+    return file;
+}
+
 std::ifstream& operator>>(std::ifstream& file, Data::Translator& translator) {
     file >> translator.name;
-    file >> translator.availableTimePeriod.start.hour;
-    file >> translator.availableTimePeriod.start.minute;
-    file >> translator.availableTimePeriod.end.hour;
-    file >> translator.availableTimePeriod.end.minute;
+    file >> translator.availableTimePeriod.start >> translator.availableTimePeriod.end;
     std::string languagesNames;
     std::getline(file, languagesNames);
     translator.languages = split(languagesNames.substr(1));  // cause we have an extra space in thr beginning
     translator.numOfLanguages = translator.languages.size();
+    return file;
+}
+
+std::ifstream& operator>>(std::ifstream& file, Data::Event& event) {
+    file >> event.name;
+    file >> event.timePeriod.start >> event.timePeriod.end;
+    std::string languagesNames;
+    std::getline(file, languagesNames);
+    event.languages = split(languagesNames.substr(1));  // cause we have an extra space in thr beginning
     return file;
 }
 
@@ -142,6 +179,44 @@ int search(const Data::Languages& languages, const std::string& name) {
     } else {
         return LANGUAGE_NOT_FOUND;
     }
+}
+
+bool language_of_event_compare(const Data::EventsLanguage& first, const Data::EventsLanguage& second) {
+    return first.numOfTranslators < second.numOfTranslators;
+}
+
+bool language_of_event_compare_for_priority(const Data::EventsLanguage& first, const Data::EventsLanguage& second) {
+    return first.priority < second.priority;
+}
+
+bool is_before(Data::Time first, Data::Time second) {
+    return (first.hour < second.hour) || ((first.hour == second.hour) && (first.minute < second.minute));
+}
+
+bool is_overlapping(Data::TimePeriod first, Data::TimePeriod second) {
+    return (is_before(second.start, first.start) || is_before(second.start, first.end)) &&
+           (is_before(first.start, second.start) || is_before(first.start, second.end));
+}
+
+bool is_available(Data::TranslatorPointer translatorPtr, Data::TimePeriod goalTimePeriod) {
+    if (is_before(goalTimePeriod.start, translatorPtr->availableTimePeriod.start) ||
+        is_before(translatorPtr->availableTimePeriod.end, goalTimePeriod.end))
+        return false;
+    for (auto element : translatorPtr->occupiedTimes)
+        if (is_overlapping(element, goalTimePeriod))
+            return false;
+    return true;
+}
+
+Data::TranslatorPointer find_first_available_translator(int languageIndex, const Data::EventScheduler& scheduler, Data::TimePeriod goalPeriod) {
+    for (int i = 0; i < scheduler.languages[languageIndex].translators.size(); i++) {
+
+        auto& translatorPtr = scheduler.languages[languageIndex].translators[i];
+    Data::TranslatorPointer translator = translatorPtr;
+    // if (is_available(, goalPeriod))
+    // return ;
+    }
+    return nullptr;
 }
 
 void schedule(Data::EventScheduler& scheduler) {
